4d4fb7a4f9a1866b9514285b8bdc38850a8f432c
Admin <admin@Admins-MacBook-Pro.local>
2023-04-17 16:15:22 -0400

    WIP

--- src/App.java
+++ src/App.java
@@ -2,51 +2,57 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        gameLoop();
+    }
+
+    public static PetNeeds birthMenu () {        
         Scanner input = new Scanner(System.in);
         System.out.println("Greetings, young one... welcome to the world. By what name shall your friends call you?");
         String petName = input.nextLine();
 
-        PetNeeds pet = new PetNeeds(petName);
-        
-        birthMenu(pet);
-        input.close();
-    }
-
-    public static void birthMenu (PetNeeds newPet) {
         System.out.println(
-            "It's a pleasure to meet you, " + newPet.getName() + "! I hope you will care for yourself during your time in this place...\n" +
+            "It's a pleasure to meet you, " + petName + "! I hope you will care for yourself during your time in this place...\n" +
             "\n" +
             "\n" +
             "\n" +
             "\n" +
             "\n" 
         );
+    
+        PetNeeds pet = new PetNeeds(petName);
+
+        input.close();
+
+        return pet;
+
     }
 
-    public static void gameLoop (PetNeeds newPet) {
+    public static void gameLoop () {
         int userSelection = -1; 
-        Scanner input2 = new Scanner(System.in);
 
-        while (userSelection != 0) {
+        while(userSelection != 0){
+            PetNeeds pet = birthMenu();
+            System.out.println(pet);
+            // Scanner action = new Scanner(System.in);
             System.out.println(
-                "What would you like to do?" +
-                "1. Feed Pet" +
-                "2. Water Pet" +
-                "0. Exit"
+                "What would you like " + pet.getName() + " to do?" + "\n" +
+                "1. Feed Pet" + "\n" +
+                "2. Water Pet" + "\n" + 
+                "0. Exit" + "\n"
                 );
-
-            userSelection = input2.nextInt();
-            switch (userSelection) {
-                case 1: newPet.feedPet();
-                        break;
-                case 2: newPet.drinkPet();
-                        break;
-                case 0: userSelection = 0;
-                        input2.close();
-                        break;
-                default: break;
-            }
-            newPet.tick();
+                
+            
+            // switch (input) {
+            //     case "1": pet.feedPet();
+            //             break;
+            //     case "2": pet.drinkPet();
+            //             break;
+            //     case "0": userSelection = 0;
+            //             action.close();
+            //             break;
+            //     default: break;
+            // }
+            pet.tick();
         }
     }
 }
