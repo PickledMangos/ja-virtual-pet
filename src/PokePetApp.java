1f6e21ebfd083a3fec2eed09b00080a8051b7a89
ItzSpooki <cjwhite101@icloud.com>
2023-04-25 18:28:25 -0400

    Added while loops and made it so we can name our pet.

--- src/PokePetApp.java
+++ src/PokePetApp.java
@@ -1,19 +1,47 @@
 import java.util.Scanner;
+//Sc7SFnwW - password for accenture
+import javax.lang.model.element.Name;
 
 public class PokePetApp {
 
+    public static boolean nameCheck(String name){
+
+        int length = name.length();
+        if (length >= 3 && length <= 10){
+            return false;
+        }
+        System.out.println("Rejected, your pets name is too long/too short (min 3 characters and max 10)");
+        return true;
+    }
     public static void main(String[] arg) throws Exception {
         Scanner input = new Scanner(System.in);
-        System.out.println("\n\nEnter your Pet Name ");
+        System.out.print("\n\nEnter your Pet Name: ");
         String name = input.nextLine();
-        MyPokePetNeeds pet = new MyPokePetNeeds(name);
-            if(name.length() > 7) {
-                System.out.println("Rejected, your name is too long.");
-            } 
-            else if(name.length() < 3) {
-                System.out.println("Rejected, your name is too short");
-            } else {
-                System.out.println("\n"+pet);
+        while(nameCheck(name)) {
+            System.out.print("Enter your Pet Name: ");
+            name = input.nextLine();
         }
+        MyPokePetNeeds pet = new MyPokePetNeeds(name);
+        // do {
+        //     System.out.println("\n\nEnter your Pet Name ");
+        //     String name = input.nextLine();
+        //     MyPokePetNeeds pet = new MyPokePetNeeds(name);
+        //         if(name.length() > 10) {
+        //             System.out.println("Rejected, your pets name is too long.");
+        //             acceptName = false;
+        //         } 
+        //         else if(name.length() < 3) {
+        //             System.out.println("Rejected, your pets name is too short");
+        //             acceptName = false;
+        //         } else {
+        //             System.out.println("\n"+name);
+        //             acceptName = true;
+        //         }
+        //} while(acceptName == false);
+        
+        System.out.println("Greetings, " + pet.getName() + "! Welcome to pokepet! Here you can raise your very own pet!" 
+            + " Choose your pet of the 3 options we give you and you can watch it grow to love you as its owner."
+            + " Once enough favorability is reached. The pet will evolve and gain unique ways to play with you.");
+
     }
 }
